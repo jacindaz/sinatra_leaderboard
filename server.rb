8e3d337c7e069fea5378f34483d44340b7f1f06a
Jacinda Zhong <jacindaz@gmail.com>
2014-05-25 13:23:54 -0400

    Remove dependencies on extract_keys method
     - was previously teams(array_of_hashes) method
     - changed to take any array of hashes and a list of keys to "extract"
     - used splat operator to take any number of keys to extract
     - this way, any array of hashes can be passed in, and the value for
       any given list of keys can be extracted and saved into a new array

--- server.rb
+++ server.rb
@@ -29,19 +29,18 @@ def losing_team(hash, team1, team2, team1_score, team2_score)
   end
 end
 
-def teams(array_of_hashes)
-  teams = []
+def extract_keys(array_of_hashes, *keys_to_extract)
+  array_extracted_values = []
   array_of_hashes.each do |game|
-    home_team = game[:home_team]
-    away_team = game[:away_team]
-    if !teams.include?(home_team)
-      teams << home_team
-    elsif !teams.include?(away_team)
-      teams << away_team
-    end
-  end
-  return teams
+    keys_to_extract.each do |key|
+      if !array_extracted_values.include?(game[key])
+        array_extracted_values << game[key]
+      end
+    end #end key array
+  end #end array of hashes
+  return array_extracted_values
 end
+#extract_keys([array of hashes of games], [:home_team, :away_team])
 
 def create_hash_keys(array_of_keys, value)
   new_hash = {}
@@ -56,7 +55,7 @@ def add_teams(array_of_teams, keys_array, key_to_update, default_key_value)
   array = []
   array_of_teams.each do |team|
     team_hash = create_hash_keys(keys_array, default_key_value)
-    team = team.capitalize
+    team = team.to_s.capitalize
     team_hash[key_to_update] = team
     array << team_hash
   end
@@ -85,9 +84,9 @@ def add_wonlost_data(array_of_teams, array_of_games)
 end
 
 #return a specific team's hash
-def one_team(array_of_teams, team_name)
+def one_team(array_of_teams, team_name, team_key)
   array_of_teams.each do |team|
-    if team[:team] == team_name
+    if team[team_key] == team_name
       return team
     end
   end
@@ -99,7 +98,7 @@ end
 get '/leaderboard' do
   @title = "Leaderboard"
   @leaderboard_array = load_csv("scores.csv")
-  @teams_array = teams(@leaderboard_array)
+  @teams_array = extract_keys(@leaderboard_array, :home_team, :away_team)
   @teams_wins_losses = add_teams(@teams_array, [:team, :wins, :losses], :team, 0)
 
   @teams_update_wins_losses = add_wonlost_data(@teams_wins_losses, @leaderboard_array)
@@ -107,19 +106,21 @@ get '/leaderboard' do
   erb :index
 end
 
+
 get '/' do
   redirect '/leaderboard'
 end
+
 
 get '/teams/:team_name' do
   @title = "Teams Page"
   @team_name = params[:team_name]
   @leaderboard_array = load_csv("scores.csv")
-  @teams_array = teams(@leaderboard_array)
+  @teams_array = extract_keys(@leaderboard_array, :home_team, :away_team)
   @teams_wins_losses = add_teams(@teams_array, [:team, :wins, :losses], :team, 0)
   @teams = add_wonlost_data(@teams_wins_losses, @leaderboard_array)
 
-  @team_hash = one_team(@teams, @team_name)
+  @team_hash = one_team(@teams, @team_name, :team)
 
   erb :show
 end
